40cb3766561e3d57e7de91c14896287a2109487b
lakshay <lakshayangzom@gmail.com>
2024-07-17 09:40:27 +0600

    final i hope

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
   fixturesFolder: "./cypress/fixtures",
   modifyObstructiveCode: false,
   video: false,
-  chromeWebSecurity: false,
   viewportHeight: 1080,
   viewportWidth: 1920,
   watchForFileChanges: false,
